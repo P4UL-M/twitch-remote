8e41040a73bf72dff9e205d6e4c252b75e6073f9
root <paul.mairesse@free.fr>
2020-12-28 14:03:50 +0000

    phase 3.2.2 : correction de petit bug du à la suppression des ligne compromettante

--- twitch-bot/TwitchChat.py
+++ twitch-bot/TwitchChat.py
@@ -41,7 +41,7 @@ class Bot():
             # si connecté alors run en continu
             if ':le_picard_fr!le_picard_fr@le_picard_fr.tmi.twitch.tv JOIN #le_picard_fr' in str(reponse):
                 message = "le ChatBot est actuellement disponible !"
-                await websocket.send(f'PRIVMSG #{CHAT} :{message}')
+                await websocket.send(f'PRIVMSG #{self.CHAT} :{message}')
 
                 while(True):
                     time.sleep(0.5)
@@ -57,22 +57,22 @@ class Bot():
                     # si commande detectée
                     elif '!help' in str(reponse):
                         message = "bienvenu sur le bot de la chaîne, les commandes disponnibles sont : !discord (affiche le lien vers le discord de la chaîne), !viewers (affiche les viewers actuels)"
-                        await websocket.send(f'PRIVMSG #{CHAT} :{message}')
+                        await websocket.send(f'PRIVMSG #{self.CHAT} :{message}')
                         print(f"Envoie de la réponse ...")
                     elif '!discord' in str(reponse):
                         message = "https://discord.gg/jBYp2s6"
-                        await websocket.send(f'PRIVMSG #{CHAT} :{message}')
+                        await websocket.send(f'PRIVMSG #{self.CHAT} :{message}')
                         print(f"Envoie de la réponse ...")
                     elif '!viewers' in str(reponse):
                         #message = TwitchRequest.get_viewers()
                         message = "en construction, ..."
-                        await websocket.send(f'PRIVMSG #{CHAT} :{message}')
+                        await websocket.send(f'PRIVMSG #{self.CHAT} :{message}')
                         print(f"Envoie de la réponse ...")
 
     # test pour quitter le chat
     async def tearsdown(self):
         # demande de désabonnement
-        await self.socket.send(f'PRIVMSG #{CHAT} :Deconnection en cours ...')
+        await self.socket.send(f'PRIVMSG #{self.CHAT} :Deconnection en cours ...')
         await self.socket.send(f'PART #{self.CHAT}')
         print(f"Déconnexion du chat ...")
 
